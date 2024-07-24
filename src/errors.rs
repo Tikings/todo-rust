07c7b4fb11cc908bfe541bf101f86d0b948e01a5
Tikings <timothee.sanchez.faly@gmail.com>
2024-07-24 09:03:58 +0200

    Adding NotAPriority Error and impl Display for TodoFileError
    Display function for each errors that might occur.
    NotAPriority -> Raised in case the priority input from the add
    subcommand is unable to be parsed

--- src/errors.rs
+++ src/errors.rs
@@ -28,5 +28,20 @@ pub enum TodoFileError {
     ClearingError,
     CopyError,
     WriteError(serde_json::Error),
-    Modify(String)
+    Modify(String),
+    NotAPriority(String),
+}
+
+impl Display for TodoFileError {
+    fn fmt(&self, f: &mut fmt::Formatter ) -> fmt::Result {
+        match *self {
+            // TodoFileError::OpenFile(e) => write!(f, "Error while opening the save file : {} ", e),
+            TodoFileError::OpenFile(_) => write!(f, "Error while opening the save file "),
+            TodoFileError::ClearingError => write!(f,"Error while clearing the file."),
+            TodoFileError::CopyError => write!(f,"Error while copying the file"),
+            TodoFileError::WriteError(_) => write!(f,"Error while parsing the file"),
+            TodoFileError::Modify(_) => write!(f,"Error while modifying the file"),
+            TodoFileError::NotAPriority(_) => write!(f,"This is not a priority : you should choose between : High, Medium or Low"),
+        }
+    } 
 }
